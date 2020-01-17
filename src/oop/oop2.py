8423edddac3a6d3cabe1fd27c5860f03d38c4eca
Nick Gonzalez <gonzaleznick5@gmail.com>
2020-01-17 12:41:20 -0500

    comp.py_Complete

--- src/oop/oop2.py
+++ src/oop/oop2.py
@@ -7,7 +7,7 @@ class GroundVehicle():
     def __init__(self, num_wheels=4):
         self.num_wheels = num_wheels
     def drive(self):
-        return "vroom"
+        return "vroooom"
     # TODO
 
 
@@ -24,7 +24,7 @@ class Motorcycle(GroundVehicle):
     def __init__(self, num_wheels=2):
         super().__init__(num_wheels)
     def drive(self):
-        return "BRAAP!!"
+        return "BRAAAP!!"
 
 # TODO
 
