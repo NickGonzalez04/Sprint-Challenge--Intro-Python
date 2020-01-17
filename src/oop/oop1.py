f25f6820d7b507b026ee444217a35c4124abc80a
Nick Gonzalez <gonzaleznick5@gmail.com>
2020-01-17 11:18:31 -0500

    ooP.py_Complete

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -23,13 +23,31 @@ class Vehicle:
 
 class GroundVehicle(Vehicle):
     def __init__(self):
-    super().__init__()
+        super().__init__()
         pass
 
 class Car(GroundVehicle):
     def __init(self):
+        super().__init__()
         pass
 
 class Motorcycle(GroundVehicle):
     def __init__(self):
+        super().__init__()
+        pass
+
+
+class FlightVehicle(Vehicle):
+    def __init__(self):
+        super().__init__()
+        pass
+
+class Airplane(FlightVehicle):
+    def __init__(self):
+        super().__init__()
+        pass
+
+class Starship(FlightVehicle):
+    def __init__(self):
+        super().__init__()
         pass
