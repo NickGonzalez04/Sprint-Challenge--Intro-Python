d77ff9f9b92c972caf10f61ff1254a6545a7a08b
Nick Gonzalez <gonzaleznick5@gmail.com>
2020-01-17 11:11:24 -0500

    First_Commit

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -17,3 +17,19 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+class Vehicle:
+    def __init__(self):
+        pass
+
+class GroundVehicle(Vehicle):
+    def __init__(self):
+    super().__init__()
+        pass
+
+class Car(GroundVehicle):
+    def __init(self):
+        pass
+
+class Motorcycle(GroundVehicle):
+    def __init__(self):
+        pass
